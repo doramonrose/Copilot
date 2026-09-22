cd1af507c950fff2f92d1d37b376ce51021155ed
Nanlaphat Thongdee <96970289+doramonrose@users.noreply.github.com>
2026-09-22 07:55:29 +0000

    Point frontend to latest Apps Script deployment

--- Canteen/frontend/config.js
+++ Canteen/frontend/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = Object.freeze({
   liffId: '2011641560-B6tYz1Vc',
-  apiUrl: 'https://script.google.com/macros/s/AKfycbzTexVVSIMP4ueVu-hup_VkQvmvxZx1O2kBMUhQvxKB4NIei1jeBIVKQV0RGK3P9RBQJA/exec'
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxsY-dsxNQMjHRZ1stEZW2hopzCALKmqDOD1acOEEyMSRaC7OkXe9sFNPh1Gq_-G2vGag/exec'
 });
