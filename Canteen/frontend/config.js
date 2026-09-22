3cc149493148b5e7f8ea6d3595730ea84ca3bd72
Nanlaphat Thongdee <96970289+doramonrose@users.noreply.github.com>
2026-09-22 03:57:50 +0000

    Deploy Canteen frontend to GitHub Pages

---
+++ Canteen/frontend/config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = Object.freeze({
+  liffId: '2011641560-B6tYz1Vc',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzTexVVSIMP4ueVu-hup_VkQvmvxZx1O2kBMUhQvxKB4NIei1jeBIVKQV0RGK3P9RBQJA/exec'
+});
